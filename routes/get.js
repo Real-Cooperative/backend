8fbb2bff7f75d68cd0b15a2f072196cd03d379de
June Pratt <juneprattm@gmail.com>
2023-07-06 19:50:33 -0400

    paginate get results

--- routes/get.js
+++ routes/get.js
@@ -8,10 +8,16 @@ const db = new Surreal('http://127.0.0.1:8000/rpc');
 const externalRequest = async (body) => {
 
     try {
-
+        let id = body.id;
+        let limit = body.limit || 10;
+        let page = body.page || 1;
+        let data = {};
         await db.signin({user: 'root', pass: 'root',});
 		await db.use('test', 'test');
-        const data = await db.select(body.id);
+        let pageQuery = await db.query(`SELECT * FROM ${id} LIMIT ${limit} START ${(page-1) * limit}`);
+        data.page = pageQuery[0].result;
+        let countQuery = await db.query(`SELECT count() FROM ${id} GROUP ALL`);
+        data.count = countQuery[0].result[0].count;
         return data;
 
 
