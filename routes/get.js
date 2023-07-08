64a4351cbf62aa374273ed1132e4f96891a539ed
June Pratt <juneprattm@gmail.com>
2023-07-08 10:52:44 -0400

    update surrealdb.js for merge

--- routes/get.js
+++ routes/get.js
@@ -1,31 +1,28 @@
-import Surreal from 'surrealdb.js';
+import Surreal from "surrealdb.js";
 
-
-const db = new Surreal('http://127.0.0.1:8000/rpc');
-
+const db = new Surreal("http://127.0.0.1:8000/rpc");
 
 //Get request to SurrealDB
 const externalRequest = async (body) => {
-
     try {
         let id = body.id;
         let limit = body.limit || 10;
         let page = body.page || 1;
         let data = {};
-        await db.signin({user: 'root', pass: 'root',});
-		await db.use('test', 'test');
-        let pageQuery = await db.query(`SELECT * FROM ${id} LIMIT ${limit} START ${(page-1) * limit}`);
+        await db.signin({ user: "root", pass: "root" });
+        await db.use({ ns: "test", db: "test" });
+        let pageQuery = await db.query(
+            `SELECT * FROM ${id} LIMIT ${limit} START ${(page - 1) * limit}`
+        );
         data.page = pageQuery[0].result;
         let countQuery = await db.query(`SELECT count() FROM ${id} GROUP ALL`);
-        data.count = countQuery[0].result[0].count;
+        data.count = countQuery[0].result[0]
+            ? countQuery[0].result[0].count
+            : 0;
         return data;
-
-
     } catch (e) {
-
-        console.error('ERROR', e);
-
+        console.error("ERROR", e);
     }
-}
+};
 
-export {externalRequest};
+export { externalRequest };
--- routes/post.js
+++ routes/post.js
@@ -1,50 +1,53 @@
-import Surreal from 'surrealdb.js';
+import Surreal from "surrealdb.js";
 
-
-const db = new Surreal('http://127.0.0.1:8000/rpc');
+const db = new Surreal("http://127.0.0.1:8000/rpc");
 
 const externalRequest = async (body) => {
     try {
-        let id = `${(body.type).replaceAll(' ', '_')}:${(body.name).replaceAll(' ', '_')}`;
-    	await db.signin({user: 'root', pass: 'root',});
-		await db.use('test', 'test');
+        let id = `${body.type.replaceAll(" ", "_")}:${body.name.replaceAll(
+            " ",
+            "_"
+        )}`;
+        await db.signin({ user: "root", pass: "root" });
+        await db.use({ ns: "test", db: "test" });
         if (await db.select(id)) {
             id = `${id}_${Date.now()}`;
         }
 
         for (let property in body) {
             if (Array.isArray(body[property])) await arrayLoop(property);
-            else if (typeof body[property] === 'object') await relateObj(body[property]);
+            else if (typeof body[property] === "object")
+                await relateObj(body[property]);
         }
 
         await db.create(id, body);
-        return { message: 'Success', id}
+        return { message: "Success", id };
 
         async function arrayLoop(property) {
             for (let obj of body[property]) {
                 await relateObj(obj);
             }
         }
-    
+
         async function relateObj(obj) {
             if (!obj.type || !obj.name) return;
-            let objID = `${(obj.type).replaceAll(' ', '_')}:${(obj.name).replaceAll(' ', '_')}`;
+            let objID = `${obj.type.replaceAll(" ", "_")}:${obj.name.replaceAll(
+                " ",
+                "_"
+            )}`;
             obj.id = objID;
             const objData = await db.select(objID);
-            if (!objData) {
+            if (objData.length === 0) {
                 await db.create(objID, { name: obj.name, [body.type]: [id] });
                 return;
-            } 
+            }
             objData[body.type].push(id);
             await db.update(objID, objData);
         }
+    } catch (e) {
+        console.error("ERROR", e);
+        return { message: "Error", e: e.message };
+    }
+};
 
-	} catch (e) {
-
-		console.error('ERROR', e);
-        return { message: 'Error', e: e.message}
-
-	}
-}
-
-export {externalRequest};
+export { externalRequest };
