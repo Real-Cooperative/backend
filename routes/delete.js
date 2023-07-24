a9735957a809a9eb4a3d74734ebd43430e38e882
June Pratt <juneprattm@gmail.com>
2023-07-24 17:55:47 -0400

    delete record

---
+++ routes/delete.js
@@ -0,0 +1,33 @@
+import Surreal from "surrealdb.js";
+import dotenv from "dotenv";
+import { parseJwt } from "../methods/parseJwt.js";
+
+dotenv.config();
+
+const db = new Surreal(process.env.SURREAL_DB || "http://localhost:8000/rpc");
+
+const delRecord = async (body, headers) => {
+    try {
+        const { id } = body;
+        if (!id) throw new Error("No ID provided");
+        const { authentication } = headers;
+        if (!authentication) throw new Error("No authentication provided");
+        const { ID } = parseJwt(authentication);
+        if (!ID) throw new Error("No ID provided in authentication");
+        await db.authenticate(authentication);
+
+        let [record] = await db.select(id);
+
+        if (!record) throw new Error(`${id} does not exist`);
+        if (record.created_by !== ID)
+            throw new Error("You do not have permission to delete this record");
+
+        await db.delete(id);
+        await db.query(`DELETE made_of WHERE in=${id}`);
+        return { status: "OK", message: `${id} was deleted` };
+    } catch (e) {
+        return { status: "Error", message: e.message };
+    }
+};
+
+export { delRecord };
