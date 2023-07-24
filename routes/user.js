7ad0972604be775187997f410f01a33b978ccf09
June Pratt <juneprattm@gmail.com>
2023-07-24 17:53:36 -0400

    update user

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ import Surreal from "surrealdb.js";
 import dotenv from "dotenv";
 import crypto from "crypto";
 import { createHash } from "crypto";
+import { parseJwt } from "../methods/parseJwt.js";
 
 dotenv.config();
 
@@ -63,7 +64,6 @@ async function signup(req) {
             const salt = crypto.randomBytes(64).toString("base64");
             const hash = hmacSHA256(data, salt).toString();
             const time = new Date().toISOString();
-
             const token = await db.signup({
                 NS: "test",
                 DB: "test",
@@ -85,13 +85,16 @@ async function signup(req) {
     }
 }
 
-async function getMe(req) {
-    const { token, username } = req;
+async function getMe(req, headers) {
+    const { authentication } = headers;
     try {
-        await db.authenticate(token);
+        await db.authenticate(authentication);
+        await db.use({ ns: "test", db: "test" });
+
+        let id = parseJwt(authentication).ID;
 
         let query = await db.query(
-            `SELECT *, "" as pass, "" as salt FROM user WHERE user = "${username}"`
+            `SELECT *, "" as pass, "" as salt FROM ${id}`
         );
 
         let details = query[0].result[0];
@@ -110,4 +113,72 @@ async function getMe(req) {
     }
 }
 
-export { login, signup, getMe };
+async function getUser(req, headers) {
+    const id = headers["x-rciad-requested-id"];
+    try {
+        await db.signin({ user: "root", pass: "root" });
+        await db.use({ ns: "test", db: "test" });
+
+        let query = await db.query(
+            `SELECT *, "" as pass, "" as salt, "" as email, "" as settings FROM user WHERE user = '${id}'`
+        );
+
+        let details = query[0].result[0];
+
+        Object.keys(details).forEach(
+            (key) => details[key] === "" && delete details[key]
+        );
+
+        return {
+            status: "OK",
+            details: details,
+            message: "Success",
+        };
+    } catch (e) {
+        return { status: "Error", details: null, message: e.message };
+    }
+}
+
+async function updateUser(req, headers) {
+    const { authentication } = headers;
+    let { email, settings, subscriptions, username } = req;
+    try {
+        await db.authenticate(authentication);
+        await db.use({ ns: "test", db: "test" });
+
+        let id = parseJwt(authentication).ID;
+
+        let userDetails = await db.query(`SELECT * FROM ${id}`);
+
+        let oldEmail = userDetails[0].result[0].email;
+        let oldSettings = userDetails[0].result[0].settings;
+        let oldSubscriptions = userDetails[0].result[0].subscriptions;
+        let oldUser = userDetails[0].result[0].user;
+
+        email = email ? email : oldEmail;
+        settings = settings ? settings : oldSettings;
+        subscriptions = subscriptions
+            ? subscriptions
+            : oldSubscriptions
+            ? oldSubscriptions
+            : [];
+        username = username ? username : oldUser;
+
+        await db.merge(id, {
+            email,
+            settings,
+            subscriptions,
+            user: username,
+        });
+
+        return {
+            status: "OK",
+            message: "Success",
+        };
+    } catch (e) {
+        console.error(e);
+        return { status: "Error", message: e.message };
+    }
+}
+
+export { login, signup, getMe, getUser, updateUser };
