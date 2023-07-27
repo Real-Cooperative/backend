8e0594c78f121f680fc4753b9246da0bbc121e0f
June Pratt <juneprattm@gmail.com>
2023-07-27 15:10:55 -0400

    remove cluster

--- index.js
+++ index.js
@@ -22,112 +22,108 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-if (cluster.isPrimary) {
-    let numWorkers = os.cpus().length || 2;
-    console.log(`Master is setting up ${numWorkers} workers`);
+// if (cluster.isPrimary) {
+//     let numWorkers = os.cpus().length || 2;
+//     console.log(`Master is setting up ${numWorkers} workers`);
 
-    for (let i = 0; i < numWorkers; i++) {
-        cluster.fork();
-    }
+//     for (let i = 0; i < numWorkers; i++) {
+//         cluster.fork();
+//     }
 
-    cluster.on("online", (worker) => {
-        console.log(`Worker ${worker.process.pid} is online`);
-    });
+//     cluster.on("online", (worker) => {
+//         console.log(`Worker ${worker.process.pid} is online`);
+//     });
 
-    cluster.on("exit", (worker, code, signal) => {
-        console.log(
-            `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`
-        );
-        console.log("Starting a new worker");
-        cluster.fork();
-    });
-} else {
-    const routes = {
-        "/login": (req, res) => use(req, res, login),
-        "/signup": (req, res) => use(req, res, signup),
-        "/me": (req, res) => use(req, res, getMe),
-        "/user": (req, res) => use(req, res, getUser),
-        "/update-user": (req, res) => use(req, res, updateUser),
-        "/post": (req, res) => use(req, res, post),
-        "/get": (req, res) => use(req, res, get),
-        "/get-relation": (req, res) => use(req, res, getRelation),
-        "/delete": (req, res) => use(req, res, delRecord),
-        "/upload": (req, res) => upload(req, res),
-        "/health": (req, res) => {
-            res.writeHead(200);
-            res.end("OK");
-        },
-    };
+//     cluster.on("exit", (worker, code, signal) => {
+//         console.log(
+//             `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`
+//         );
+//         console.log("Starting a new worker");
+//         cluster.fork();
+//     });
+// } else {
+const routes = {
+    "/login": (req, res) => use(req, res, login),
+    "/signup": (req, res) => use(req, res, signup),
+    "/me": (req, res) => use(req, res, getMe),
+    "/user": (req, res) => use(req, res, getUser),
+    "/update-user": (req, res) => use(req, res, updateUser),
+    "/post": (req, res) => use(req, res, post),
+    "/get": (req, res) => use(req, res, get),
+    "/get-relation": (req, res) => use(req, res, getRelation),
+    "/delete": (req, res) => use(req, res, delRecord),
+    "/upload": (req, res) => upload(req, res),
+    "/health": (req, res) => {
+        res.writeHead(200);
+        res.end("OK");
+    },
+};
 
-    const server = app.createServer(async (req, res) => {
-        res.setHeader(
-            "Access-Control-Allow-Origin",
-            process.env.WHITE_LISTED_DOMAIN
-        );
+const server = app.createServer(async (req, res) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        process.env.WHITE_LISTED_DOMAIN
+    );
 
-        res.setHeader(
-            "Access-Control-Allow-Methods",
-            "POST, GET, DELETE, PATCH"
-        );
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-        res.setHeader("Access-Control-Max-Age", 86400);
-        res.setHeader(
-            "Access-Control-Allow-Headers",
-            "Authentication, X-RCIAD-Requested-ID, x-rciad-requested-user, x-rciad-page, x-rciad-limit, x-rciad-requested-relation, x-rciad-subscribed"
-        );
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Max-Age", 86400);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Authentication, X-RCIAD-Requested-ID, x-rciad-requested-user, x-rciad-page, x-rciad-limit, x-rciad-requested-relation, x-rciad-subscribed"
+    );
 
-        if (req.url in routes) {
-            return routes[req.url](req, res);
-        } else {
-            const parsedUrl = url.parse(req.url);
-            // extract URL path
-            let pathname = `.${parsedUrl.pathname}`;
-            // based on the URL path, extract the file extension. e.g. .js, .doc, ...
-            const ext = path.parse(pathname).ext;
-            // maps file extension to MIME typere
-            const map = {
-                ".ico": "image/x-icon",
-                ".html": "text/html",
-                ".js": "text/javascript",
-                ".json": "application/json",
-                ".css": "text/css",
-                ".png": "image/png",
-                ".jpg": "image/jpeg",
-                ".wav": "audio/wav",
-                ".mp3": "audio/mpeg",
-                ".svg": "image/svg+xml",
-                ".pdf": "application/pdf",
-                ".doc": "application/msword",
-            };
+    if (req.url in routes) {
+        return routes[req.url](req, res);
+    } else {
+        const parsedUrl = url.parse(req.url);
+        // extract URL path
+        let pathname = `.${parsedUrl.pathname}`;
+        // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+        const ext = path.parse(pathname).ext;
+        // maps file extension to MIME typere
+        const map = {
+            ".ico": "image/x-icon",
+            ".html": "text/html",
+            ".js": "text/javascript",
+            ".json": "application/json",
+            ".css": "text/css",
+            ".png": "image/png",
+            ".jpg": "image/jpeg",
+            ".wav": "audio/wav",
+            ".mp3": "audio/mpeg",
+            ".svg": "image/svg+xml",
+            ".pdf": "application/pdf",
+            ".doc": "application/msword",
+        };
 
-            fs.stat(pathname, function (err, exist) {
+        fs.stat(pathname, function (err, exist) {
+            if (err) {
+                // if the file is not found, return 404
+                res.statusCode = 404;
+                res.end(`File ${pathname} not found!`);
+                return;
+            }
+
+            // if is a directory search for index file matching the extension
+            if (fs.statSync(pathname).isDirectory()) pathname += "/index" + ext;
+
+            // read file from file system
+            fs.readFile(pathname, function (err, data) {
                 if (err) {
-                    // if the file is not found, return 404
-                    res.statusCode = 404;
-                    res.end(`File ${pathname} not found!`);
-                    return;
+                    res.statusCode = 500;
+                    res.end(`Error getting the file: ${err}.`);
+                } else {
+                    // if the file is found, set Content-type and send data
+                    res.setHeader("Content-type", map[ext] || "text/plain");
+                    res.end(data);
                 }
-
-                // if is a directory search for index file matching the extension
-                if (fs.statSync(pathname).isDirectory())
-                    pathname += "/index" + ext;
-
-                // read file from file system
-                fs.readFile(pathname, function (err, data) {
-                    if (err) {
-                        res.statusCode = 500;
-                        res.end(`Error getting the file: ${err}.`);
-                    } else {
-                        // if the file is found, set Content-type and send data
-                        res.setHeader("Content-type", map[ext] || "text/plain");
-                        res.end(data);
-                    }
-                });
             });
-        }
-    });
+        });
+    }
+});
 
-    server.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-    });
-}
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+// }
