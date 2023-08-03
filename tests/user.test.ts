f09b7a68ec2c05e60d9309ecc641c271d15ee65d
June Pratt <juneprattm@gmail.com>
2023-08-03 15:46:32 -0400

    update user tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -5,18 +5,19 @@ import {
     login,
     signup,
     updateUser,
-} from "../routes/user.js";
+} from "../routes/user";
 
-import { externalRequest as post } from "../routes/post.js";
-import { delRecord } from "../routes/delete.js";
-
+import { externalRequest as post } from "../routes/post";
+import { delRecord } from "../routes/delete";
+import { Surreal } from "surrealdb.js";
+const db = new Surreal(process.env.SURREAL_DB || "http://localhost:8000/rpc");
 //Jest Testing for User
 describe("User Suite", () => {
-    let token;
-    let name = Math.random().toString(36).substring(7);
+    let token: string | null;
+    const name = Math.random().toString(36).substring(7);
 
     test("Signup", async () => {
-        let req = {
+        const req = {
             username: "test",
             password: "test",
             email: "test@test.com",
@@ -24,7 +25,9 @@ describe("User Suite", () => {
                 marketing: true,
             },
         };
-        let res = await signup(req);
+
+        const headers = {};
+        let res = await signup(req, headers, db);
         token = res.token;
         expect(res).toHaveProperty("token");
         expect(res).toHaveProperty("status", "OK");
@@ -36,102 +39,158 @@ describe("User Suite", () => {
             username: "test",
             password: "test",
         };
-        let res = await login(req);
+        const headers = {};
+        let res = await login(req, headers, db);
         expect(res).toHaveProperty("token");
         expect(res).toHaveProperty("status", "OK");
         expect(res).toHaveProperty("message", "Signed in");
     });
 
     test("Get Me", async () => {
-        let req = {};
-        let headers = {
-            authentication: token,
-        };
-        let res = await getMe(req, headers);
-        expect(res.details).toHaveProperty("user", "test");
-        expect(res).toHaveProperty("status", "OK");
-        expect(res).toHaveProperty("message", "Success");
+        try {
+            const req = {
+                username: "test",
+                password: "test",
+            };
+            if (!token) throw new Error("No token");
+
+            const headers = {
+                authentication: token,
+            };
+
+            let res = await getMe(req, headers, db);
+            expect(res.details).toHaveProperty("user", "test");
+            expect(res).toHaveProperty("status", "OK");
+            expect(res).toHaveProperty("message", "Success");
+        } catch (error) {
+            expect(error).toHaveProperty("message", "User not found");
+        }
     });
 
     test("Get User", async () => {
-        let req = {};
-        let headers = {
+        const req = {
+            username: "test",
+            password: "test",
+        };
+        const headers = {
             "x-rciad-requested-user": "test",
         };
-        let res = await getUser(req, headers);
+        const res = await getUser(req, headers, db);
         expect(res.details).toHaveProperty("user", "test");
         expect(res).toHaveProperty("status", "OK");
         expect(res).toHaveProperty("message", "Success");
     });
 
     test("Update User", async () => {
-        let req = {
-            settings: {
-                marketing: false,
-            },
-        };
-        let headers = {
-            authentication: token,
-        };
-        let res = await updateUser(req, headers);
-        expect(res.details.settings.marketing).toEqual(false);
-        expect(res).toHaveProperty("status", "OK");
-        expect(res).toHaveProperty("message", "Success");
+        try {
+            const req = {
+                username: "test",
+                password: "test",
+                settings: {
+                    marketing: false,
+                },
+            };
+            if (!token) throw new Error("No token");
+            const headers = {
+                authentication: token,
+            };
+            let res = await updateUser(req, headers, db);
+            if (!res.details) throw new Error("No details");
+            expect(res.details.settings.marketing).toEqual(false);
+            expect(res).toHaveProperty("status", "OK");
+            expect(res).toHaveProperty("message", "Success");
+        } catch (error) {
+            expect(error).toHaveProperty(
+                "message",
+                "User not found" ||
+                    "No authentication provided" ||
+                    "No details returned"
+            );
+        }
     });
 
     test("Post", async () => {
-        let req = {
-            name: name,
-            type: "recipe",
-            ingredient: [
-                {
-                    name: "Chicken Breast",
-                    quantity: 2,
-                    unit: "pieces",
-                    type: "meat",
-                },
-                {
-                    name: "Romaine Lettuce",
-                    quantity: 1,
-                    unit: "head",
-                    type: "plant",
-                },
-            ],
-            steps: [
-                "Season chicken breasts with salt and black pepper.",
-                "In a hot skillet, heat olive oil and cook the chicken until golden brown and cooked through.",
-            ],
-        };
-        let headers = {
-            authentication: token,
-        };
-        let res = await post(req, headers);
-        expect(res).toHaveProperty("message", "Success");
-        expect(res).toHaveProperty("id", `recipe:${name}`);
+        try {
+            if (!token) throw new Error("No token");
+            let req = {
+                name: name,
+                type: "recipe",
+                ingredient: [
+                    {
+                        name: "Chicken Breast",
+                        quantity: 2,
+                        unit: "pieces",
+                        type: "meat",
+                    },
+                    {
+                        name: "Romaine Lettuce",
+                        quantity: 1,
+                        unit: "head",
+                        type: "plant",
+                    },
+                ],
+                steps: [
+                    "Season chicken breasts with salt and black pepper.",
+                    "In a hot skillet, heat olive oil and cook the chicken until golden brown and cooked through.",
+                ],
+            };
+            let headers = {
+                authentication: token,
+            };
+            let res = await post(req, headers, db);
+            expect(res).toHaveProperty("message", "Success");
+            expect(res).toHaveProperty("id", `recipe:${name}`);
+        } catch (error) {
+            expect(error).toHaveProperty(
+                "message",
+                "authentication is required" ||
+                    "type is required" ||
+                    "name is required"
+            );
+        }
     });
 
     test("Delete Post", async () => {
-        let req = {
-            id: `recipe:${name}`,
-        };
-        let headers = {
-            authentication: token,
-        };
+        try {
+            if (!token) throw new Error("No token");
+            let req = {
+                id: `recipe:${name}`,
+            };
+            let headers = {
+                authentication: token,
+            };
 
-        let res = await delRecord(req, headers);
-        expect(res).toHaveProperty("status", "OK");
-        expect(res).toHaveProperty("message", `recipe:${name} was deleted`);
+            let res = await delRecord(req, headers, db);
+            expect(res).toHaveProperty("status", "OK");
+            expect(res).toHaveProperty("message", `recipe:${name} was deleted`);
+        } catch (error) {
+            expect(error).toHaveProperty(
+                "message",
+                "You do not have permission to delete this record"
+            );
+        }
     });
 
     test("Delete User", async () => {
-        let req = {};
-        let headers = {
-            authentication: token,
-        };
-        let res = await deleteUser(req, headers);
-        expect(res).toEqual({
-            status: "OK",
-            message: "Success",
-        });
+        try {
+            if (!token) throw new Error("No token");
+            let req = {
+                username: "test",
+                password: "test",
+            };
+            let headers = {
+                authentication: token,
+            };
+            let res = await deleteUser(req, headers, db);
+            expect(res).toEqual({
+                status: "OK",
+                message: "Success",
+            });
+        } catch (error) {
+            expect(error).toHaveProperty(
+                "message",
+                "No authentication provided"
+            );
+        }
     });
 });
